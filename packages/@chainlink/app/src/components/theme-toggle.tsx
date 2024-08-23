7e864130e0fa3568bc7b7373a1e0c77d6daa814a
Lars-Ivar Giskegjerde <lars-ivar.giskegjerde@twoday.com>
2024-08-23 09:35:40 +0200

    rename ui to app

--- packages/@chainlink/app/src/components/theme-toggle.tsx
+++ packages/@chainlink/app/src/components/theme-toggle.tsx
@@ -1,15 +1,13 @@
-"use client"
-
 import { Moon, Sun } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/src/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useTheme } from "next-themes"
+} from "@/src/components/ui/dropdown-menu"
+import { useTheme } from "@/src/components/theme-provider"
 
 export function ThemeToggle() {
   const { setTheme } = useTheme()
--- packages/@chainlink/ui/src/components/spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-export function Spinner() {
-  return (
-    <div className="w-full h-full flex items-center justify-center">
-      <Loader2 className="animate-spin" />
-    </div>
-  );
-}
