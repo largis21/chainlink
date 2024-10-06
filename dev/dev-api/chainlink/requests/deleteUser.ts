d79ec4210006cd28441af91f9125c65d85b2cc49
Lars-Ivar Giskegjerde <lars-ivar.giskegjerde@twoday.com>
2024-10-06 21:43:52 +0200

    Refactor(core/app): removed requests-/chainsdir, all files live in chainlinkdir now

---
+++ dev/dev-api/chainlink/requests/deleteUser.ts
@@ -0,0 +1,13 @@
+import { defineRequest } from "@chainlink-io/chainlink";
+
+const url = `${cl.globals.BASE_URL}/users`;
+
+const config = defineRequest({
+  url,
+  method: "DELETE",
+  queryParams: [
+    { enabled: true, key: "Authentication", value: "Bearer 12345" },
+  ],
+});
+
+export default config;
